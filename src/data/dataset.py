Fixed reward calculation

src/data/dataset.py
@@ -166,13 +166,11 @@ class DoomStreamingDataModule(LightningDataModule):
             size = dataset_info.max_steps - self._dataset_start_index
 
             def online_policy():
-                return self.policy
+                return torch.compile(self.policy)
 
             if method != "online" and dataset_info.policy_maker:
-                policy_maker = dataset_info.policy_maker()
-                torch._dynamo.config.capture_scalar_outputs = True
-                policy = torch.compile(policy_maker)
-                torch._dynamo.config.capture_scalar_outputs = False
+                policy = dataset_info.policy_maker()
+                policy = torch.compile(policy)
             else:
                 policy = online_policy()
 

src/modules/actor.py
@@ -331,7 +331,7 @@ class LightningDecisionTransformer(L.LightningModule, TensorDictModuleBase):
         assert batch.batch_dims == 2
 
         reward = batch[("next", "reward")]
-        max_rewards = torch.stack(tuple(torch.max(t) for t in reward.unbind(0)))
+        max_rewards = torch.stack(tuple(t.cumsum(-2).max() for t in reward.unbind(0)))
         mean_max_reward = torch.mean(max_rewards)
         self.log("reward", mean_max_reward, on_step=True)
 